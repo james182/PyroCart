9d76c7915910f96631e863858ff3cec27b75624b
james lawrie <jameslawrie82@gmail.com>
2011-10-16 17:37:52 +1000

    Fixed and removed the "p" floating in top left

--- pyrocart/models/images_m.php
+++ pyrocart/models/images_m.php
@@ -1,4 +1,4 @@
-p<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Images_m extends CI_Model
 {
